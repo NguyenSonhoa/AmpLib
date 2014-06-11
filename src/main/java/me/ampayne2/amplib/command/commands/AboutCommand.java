792c70c48a816975df3f522437905869d28653bf
Austin Payne <payneaustin5@gmail.com>
2014-06-11 17:05:07 -0500

    Allow adding info to the about command

--- src/main/java/me/ampayne2/amplib/command/commands/AboutCommand.java
+++ src/main/java/me/ampayne2/amplib/command/commands/AboutCommand.java
@@ -8,13 +8,15 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.permissions.Permission;
 import org.bukkit.permissions.PermissionDefault;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * A command that lists some information about the plugin.
  */
 public class AboutCommand extends Command {
     private final String header;
-    private final String author;
-    private final String version;
+    private final List<String> info = new ArrayList<>();
 
     public AboutCommand(AmpJavaPlugin plugin) {
         super(plugin, "");
@@ -23,14 +25,24 @@ public class AboutCommand extends Command {
         setPermission(new Permission(pluginName + ".about", PermissionDefault.TRUE));
         setPlayerOnly(false);
         header = Messenger.HIGHLIGHT_COLOR + "<-------<| " + Messenger.PRIMARY_COLOR + "About " + pluginName + " " + Messenger.HIGHLIGHT_COLOR + "|>------->";
-        author = Messenger.SECONDARY_COLOR + "Author: " + StringUtils.join(plugin.getDescription().getAuthors(), ", ");
-        version = Messenger.SECONDARY_COLOR + "Version: " + plugin.getDescription().getVersion();
+        info.add(Messenger.SECONDARY_COLOR + "Author: " + StringUtils.join(plugin.getDescription().getAuthors(), ", "));
+        info.add(Messenger.SECONDARY_COLOR + "Version: " + plugin.getDescription().getVersion());
     }
 
     @Override
     public void execute(String command, CommandSender sender, String[] args) {
         sender.sendMessage(header);
-        sender.sendMessage(author);
-        sender.sendMessage(version);
+        for (String message : info) {
+            sender.sendMessage(message);
+        }
+    }
+
+    /**
+     * Adds more info to the about command.
+     *
+     * @param message The info message to add.
+     */
+    public void addInfo(String message) {
+        info.add(message);
     }
 }
